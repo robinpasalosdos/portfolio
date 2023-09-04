7d22d147671c22c1afa92402045599349d5b3f67
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2023-09-04 19:48:11 +0800

    upgraded

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ function opentab(tabname){
     }
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu_links");
+    const icon = document.querySelector(".hamburger_icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
