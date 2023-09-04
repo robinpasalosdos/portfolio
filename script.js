a39db3581175201cf69da05156ab4b095737e5de
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2023-09-04 23:17:14 +0800

    malapit na

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-
-const navigation = document.querySelector(".primary-navigation");
-
-const navigationHeight = navigation.offsetHeight;
-
-document.documentElement.style.setProperty(
-    "--scroll-padding",
-    navigationHeight + "px"
-);
-
-var tablink = document.getElementsByClassName("tab-link");
-var tabcontent = document.getElementsByClassName("tab-content")
-function opentab(tabname){
-    for(content of tabcontent){
-    content.classList.remove("active-tab");
-    }
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
 function toggleMenu() {
     const menu = document.querySelector(".menu_links");
     const icon = document.querySelector(".hamburger_icon");
