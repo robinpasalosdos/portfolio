9defb6e84b7c27d2691d3f43f9b3a713ee39be09
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2023-10-03 00:28:01 +0800

    career animation

--- old/script.js
+++ old/script.js
@@ -4,3 +4,27 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+const textElement = document.getElementById("section_text_p2");
+const careers = ["Web Developer        ",
+                  "Game Developer        ",
+                  "Mobile App Developer        ",
+                  "Data Analyst        "];
+
+let currentWordIndex = 0;
+let currentletterIndex = 1;
+
+function updateText() {
+  textElement.textContent = careers[currentWordIndex].slice(0,currentletterIndex);
+  currentletterIndex = currentletterIndex + 1;
+  if(currentWordIndex == 3 && currentletterIndex == 21){
+    currentWordIndex = 0;
+    currentletterIndex = 1;
+  }
+  if(currentletterIndex == careers[currentWordIndex].length + 1){
+    currentWordIndex = currentWordIndex + 1;
+    currentletterIndex = 1;
+  }
+}
+
+setInterval(updateText,100);
