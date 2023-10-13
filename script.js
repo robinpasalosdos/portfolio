b4ebb0cc0fada81fe9b44b29675f9bec368535c0
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2023-10-13 19:04:33 +0800

    changing files

--- script.js
+++ script.js
@@ -1,169 +1,140 @@
-var divElement1 = document.getElementById("tab-1st"); 
-var divElement2 = document.getElementById("tab-2nd"); 
-var divElement3 = document.getElementById("tab-3rd"); 
-var divElement4 = document.getElementById("tab-4th"); 
-var divElement5 = document.getElementById("tab-5th"); 
-var anchorElement1 = divElement1.querySelector("a");
-var anchorElement2 = divElement2.querySelector("a");
-var anchorElement3 = divElement3.querySelector("a");
-var anchorElement4 = divElement4.querySelector("a");
-var anchorElement5 = divElement5.querySelector("a");
+function toggleMenu() {
+    const menu = document.querySelector(".menu_links");
+    const icon = document.querySelector(".hamburger_icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
 
-var tabContainer = document.getElementById("tab-container"); 
+const textElement = document.getElementById("section_text_p2");
+const careers = ["Web Developer        ",
+                  "Game Developer        ",
+                  "Mobile App Developer        ",
+                  "Data Analyst        "];
+
+let currentWordIndex = 0;
+let currentletterIndex = 1;
+
+function updateText() {
+  textElement.textContent = careers[currentWordIndex].slice(0,currentletterIndex);
+  currentletterIndex = currentletterIndex + 1;
+  if(currentWordIndex == 3 && currentletterIndex == 21){
+    currentWordIndex = 0;
+    currentletterIndex = 1;
+  }
+  if(currentletterIndex == careers[currentWordIndex].length + 1){
+    currentWordIndex = currentWordIndex + 1;
+    currentletterIndex = 1;
+  }
+}
+
+setInterval(updateText,100);
+
+var tabContainer = document.getElementById("desktop_nav");
+var hamContainer = document.getElementById("hamburger_nav");
 var lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-var firstWork = document.getElementById("first-work");
-var secondWork = document.getElementById("second-work");
-var thirdWork = document.getElementById("third-work");
-var firstWorkDetails = document.getElementById("first-work-details");
-var secondWorkDetails = document.getElementById("second-work-details");
-var thirdWorkDetails = document.getElementById("third-work-details");
-
-
-
-anchorElement3.classList.add("scale-main");
-divElement2.classList.add("scale-2nd");
-divElement4.classList.add("scale-2nd");
-
-
-divElement1.addEventListener("click", function() {
-    forTab2();
-    forTab2();
-});
-divElement5.addEventListener("click", function() {
-    forTab4();
-    forTab4();
-});
-divElement2.addEventListener("click", forTab2);
-divElement4.addEventListener("click", forTab4);
-
-
-function forTab2(){
-    divElement2.classList.remove("scale-2nd");
-    divElement4.classList.remove("scale-2nd");
-    anchorElement3.classList.remove("scale-main");
-    divElement5.classList.add("scale-down");
-    divElement2.classList.add("slide-right");
-    divElement3.classList.add("slide-right");
-    divElement4.classList.add("slide-right");
-    divElement1.classList.add("slide-right");
-    setTimeout(function() {
-        
-        divElement5.classList.remove("scale-down");
-        var cloneElement = anchorElement5.cloneNode(true);
-        anchorElement5.href = anchorElement4.href;
-        anchorElement5.textContent = anchorElement4.textContent;
-        divElement4.classList.remove("slide-right");
-        divElement4.classList.add("scale-2nd");
-        anchorElement4.href = anchorElement3.href;
-        anchorElement4.textContent = anchorElement3.textContent;
-        divElement3.classList.remove("slide-right");
-        anchorElement3.classList.add("scale-main");
-        anchorElement3.href = anchorElement2.href;
-        anchorElement3.textContent = anchorElement2.textContent;
-        divElement2.classList.remove("slide-right");
-        divElement2.classList.add("scale-2nd");
-        anchorElement2.href = anchorElement1.href;
-        anchorElement2.textContent = anchorElement1.textContent;   
-        divElement1.classList.remove("slide-right");
-        anchorElement1.href = cloneElement.href;
-        anchorElement1.textContent = cloneElement.textContent;
-      }, 605);
+function slideDown(){
+  tabContainer.classList.add("slide-down");
+  hamContainer.classList.add("slide-down");
+  tabContainer.style.boxShadow = '0 1px 6px rgba(0, 0, 0, 0.2)';
+  hamContainer.style.boxShadow = '0 1px 6px rgba(0, 0, 0, 0.2)';
+  setTimeout(function() {
+  tabContainer.classList.remove("slide-up");
+  hamContainer.classList.remove("slide-up");
+  },100);
 }
 
-function forTab4(){
-    divElement2.classList.remove("scale-2nd");
-    divElement4.classList.remove("scale-2nd");
-    anchorElement3.classList.remove("scale-main");
-    divElement1.classList.add("scale-up");
-    divElement2.classList.add("slide-left");
-    divElement3.classList.add("slide-left");
-    divElement4.classList.add("slide-left");
-    divElement5.classList.add("slide-left");
-    setTimeout(function() {
-        divElement1.classList.remove("scale-up");
-        var cloneElement = anchorElement1.cloneNode(true);
-        anchorElement1.href = anchorElement2.href;
-        anchorElement1.textContent = anchorElement2.textContent;
-        divElement2.classList.remove("slide-left");
-        divElement2.classList.add("scale-2nd");
-        anchorElement2.href = anchorElement3.href;
-        anchorElement2.textContent = anchorElement3.textContent;
-        divElement3.classList.remove("slide-left");
-        anchorElement3.classList.add("scale-main");
-        anchorElement3.href = anchorElement4.href;
-        anchorElement3.textContent = anchorElement4.textContent;
-        divElement4.classList.remove("slide-left");
-        divElement4.classList.add("scale-2nd");
-        anchorElement4.href = anchorElement5.href;
-        anchorElement4.textContent = anchorElement5.textContent;
-        divElement5.classList.remove("slide-left");
-        anchorElement5.href = cloneElement.href;
-        anchorElement5.textContent = cloneElement.textContent;
-      }, 605);
-}
-
-function toggleMenu() {
-    const icon = document.querySelector(".span-container");
-    icon.classList.toggle("open");
-}
-
-function handleScroll() {
-  var currentScrollTop = document.documentElement.scrollTop;
-  if (currentScrollTop >= lastScrollTop) {
-    tabContainer.classList.add("slide-up");
+function slideUp(){
+  tabContainer.classList.add("slide-up");
+  hamContainer.classList.add("slide-up");
+  tabContainer.style.boxShadow = '0 1px 6px rgba(0, 0, 0, 0.2)';
+  hamContainer.style.boxShadow = '0 1px 6px rgba(0, 0, 0, 0.2)';
     setTimeout(function() {
         tabContainer.classList.remove("slide-down");
-        },200);
+        hamContainer.classList.remove("slide-down");
+        },100);
+}
+
+function navHandleScroll() {
+  var currentScrollTop = document.documentElement.scrollTop;
+  if (currentScrollTop > lastScrollTop) {
+    slideUp();
 
   }else{
-    tabContainer.classList.add("slide-down");
-    setTimeout(function() {
-    tabContainer.classList.remove("slide-up");
-    },200);
+    slideDown();
   }
   lastScrollTop = currentScrollTop;
 }
-
+  
 window.addEventListener("scroll", function() {
-    handleScroll();
-    workAnimation();
+  navHandleScroll();
+    if (window.pageYOffset == 0){
+      tabContainer.style.boxShadow = 'none';
+      hamContainer.style.boxShadow = 'none';
+    }
 });
 
-function workAnimation(){
-    if(window.pageYOffset >= 0 && window.pageYOffset < 200){
-        firstWork.classList.add("work-container-animate");
-        secondWork.classList.remove("work-container-animate");
-        thirdWork.classList.remove("work-container-animate");
-        secondWorkDetails.style.display = "none";
-        thirdWorkDetails.style.display = "none";
-        firstWorkDetails.style.display = "flex";
-          
 
-    }
+const element = document.getElementById('socials_container');
+const icons = element.querySelectorAll("img");
+icons.forEach((icon) => {
+  icon.addEventListener('mouseenter', () => {
+    icon.classList.add('scaleup');
+    setTimeout(function() {
+      icon.classList.remove('scaledown');
+      },100);
+  });
 
-    if(window.pageYOffset >= 200 && window.pageYOffset < 400){
-        firstWork.classList.remove("work-container-animate");
-        secondWork.classList.add("work-container-animate");
-        thirdWork.classList.remove("work-container-animate");
-        secondWorkDetails.style.display = "flex";
-        firstWorkDetails.style.display = "none";
-        thirdWorkDetails.style.display = "none";
-
-
-    }
-    if(window.pageYOffset >= 400 && window.pageYOffset < 600){
-        firstWork.classList.remove("work-container-animate");
-        secondWork.classList.remove("work-container-animate");
-        thirdWork.classList.add("work-container-animate");
-        thirdWorkDetails.style.display = "flex";
-        secondWorkDetails.style.display = "none";
-        firstWorkDetails.style.display = "none";
-
-    }
+  icon.addEventListener('mouseout', () => {
+    icon.classList.add('scaledown');
+    setTimeout(function() {
+      icon.classList.remove('scaleup');
+      },100);
+  });
+  
+});
+const htmlElement = document.documentElement;
+const bodyElement = document.body;
+function showDialog(dialogBox) {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById(dialogBox).style.display = "block";
+  htmlElement.style.overflow = "hidden";
+  bodyElement.style.overflow = "hidden";
 }
 
+function hideDialog(dialogBox) {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById(dialogBox).style.display = "none";
+  htmlElement.style.overflow = "auto";
+  bodyElement.style.overflow = "auto";
+}
 
-var yOffset = window.pageYOffset;
-console.log("The value of window.pageYOffset is: " + yOffset);
+document.addEventListener('click', function (event) {
+  if (event.target === document.getElementById("overlay")) {
+    hideDialog("dialog-box-1");
+    hideDialog("dialog-box-2");
+    hideDialog("dialog-box-3");
+  }
+});
+
+const sections = document.querySelectorAll('.animated-section');
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight)*3/4
+    );
+}
+
+function handleScroll() {
+    sections.forEach((section) => {
+        if (isElementInViewport(section)) {
+            section.classList.add('animated');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
+
+handleScroll();
 
