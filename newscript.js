7479361a52a164f3a848dcc62df38605cec2d5c2
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2023-10-22 00:18:58 +0800

    with cards

--- newscript.js
+++ newscript.js
@@ -4,4 +4,28 @@ const icon = document.querySelector(".hamburger_icon");
 function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+}
+
+const textElement = document.getElementById("home_text_p2");
+const careers = ["Web Developer        ",
+                  "Game Developer        ",
+                  "Mobile App Developer        ",
+                  "Data Analyst        "];
+
+let currentWordIndex = 0;
+let currentletterIndex = 1;
+
+function updateText() {
+  textElement.textContent = careers[currentWordIndex].slice(0,currentletterIndex);
+  currentletterIndex = currentletterIndex + 1;
+  if(currentWordIndex == 3 && currentletterIndex == 21){
+    currentWordIndex = 0;
+    currentletterIndex = 1;
   }
+  if(currentletterIndex == careers[currentWordIndex].length + 1){
+    currentWordIndex = currentWordIndex + 1;
+    currentletterIndex = 1;
+  }
+}
+
+setInterval(updateText,100);
