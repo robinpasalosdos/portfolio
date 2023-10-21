9fc1c4ef77cf0f08377ba0e6ab2d81ca5c257809
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2023-10-21 21:43:59 +0800

    gawa ako bago

---
+++ newscript.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".mobile_nav_sections");
+const icon = document.querySelector(".hamburger_icon");
+
+function toggleMenu() {
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
