286d356dd4f88c327a790645a18772a8ebfc76dc
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2023-11-05 22:30:04 +0800

    animation 75%

--- newscript.js
+++ newscript.js
@@ -1,41 +1,19 @@
 const menu = document.querySelector(".mobile_nav_sections");
 const icon = document.querySelector(".hamburger_icon");
-AOS.init();
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 500, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-});
 
 function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
 
+
 function goToHome() {
   var homeSection = document.getElementById('home');
   if (homeSection) {
       homeSection.scrollIntoView({ behavior: 'smooth' });
+      AOS.refreshHard();
   }
 }
-
 const elements = {
   themeToggle: document.getElementById("theme-toggle"),
   theme: document.querySelector("body"),
@@ -46,7 +24,21 @@ const elements = {
   homeProfile: document.getElementById('home_profile'),
   profileIcons: document.getElementById('socmed_card').getElementsByTagName('img'),
   contactIcons: document.getElementById('contact_strip').getElementsByTagName('img'),
-  switchLogo: document.getElementById('sun_n_moon')
+  switchLogo: document.getElementById('sun_n_moon'),
+  loading_screen: document.getElementById('loading_screen')
+};
+AOS.init({
+  disable: false,
+  startEvent: 'load',
+  delay: 0,
+  duration: 600,
+  once: false, 
+});
+
+elements.theme.style.overflow = 'hidden';
+window.onload = function() {
+  elements.loading_screen.style.display = 'none';
+  elements.theme.style.overflow = 'auto';
 };
 
 function changeImages(logo, home, nav, slogo) {
