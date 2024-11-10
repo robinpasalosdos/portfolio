45d57379bf92ba4c866060e841ce41b9a967a6f6
robpasalosdos <robin.pasalosdos@elinnovtech.com>
2024-11-10 17:59:15 +0800

    update v2.1.0

--- script.js
+++ script.js
@@ -344,6 +344,53 @@ document.addEventListener("DOMContentLoaded", function(){
   next.addEventListener('click', showNextCertificate);
   prev.addEventListener('click', showPrevCertificate);
 });
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("card");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show_card");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show_card");
+  }
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("project_links");
+var btns = btnContainer.querySelectorAll("li.project_link a");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = btnContainer.querySelectorAll("li.project_link a.active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
   
 
 
